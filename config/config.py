deb5be0929911c4967c79832b6c69b48ad21db53
sangu272 <sangeeta271713@gmail.com>
2024-11-02 11:08:20 +0530

    𝐓𝐆 𝐍𝐀𝐌𝐄

--- config/config.py
+++ config/config.py
@@ -95,7 +95,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SARKAROP123/SARKARMUSICNEW",
+    "https://github.com/sangu272/NewMUSICNEW",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
