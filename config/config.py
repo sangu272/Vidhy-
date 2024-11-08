86eebf0052203e70b7abf14d17e8381b1a3c4728
sangu272 <sangeeta271713@gmail.com>
2024-11-08 17:09:12 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/sangu272/EXTRAPLUGIN-",
+    "https://github.com/sangu272/Vidu-extra",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
