f50dec687916799787b360397fe257ab8c859018
sangu272 <sangeeta271713@gmail.com>
2024-11-08 20:42:58 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -37,7 +37,7 @@ def welcomepic(pic, user, chat, id, uname):
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize(
-        (280, 280)
+        (500, 500)
     ) 
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('assets/font.ttf', size=35)
