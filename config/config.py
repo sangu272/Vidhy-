e57a6219d571c646733cf887e8051d53eaf02d3e
⎯꯭̽𓆰𝅃꯭꯭᳚ ༎ࠫ ꯭𓆩꯭᪵𝛅͟𝗔͟ ̬𝗡᳟᪵͟𝗔᳟͟𝗧͟𝗔᳟᪵͟𝗡᳟͟𝗜 ͟𓆪᪵ꪾ ͟𝛊֟፝ؖ۬𝁘𝐁᧘ᴄʜ𝛊֟፝ؖ۬ <sangeeta271713@gmail.com>
2024-12-01 20:57:56 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -284,8 +284,12 @@ SPOTIFY_PLAYLIST_IMG_URL = getenv(
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
-
-
+    
+def seconds_to_time(seconds):
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    return f"{minutes:02d}:{remaining_seconds:02d}"
+    
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 SONG_DOWNLOAD_DURATION_LIMIT = int(time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00"))
 
