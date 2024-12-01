649923abfa9816738b9a58f5349602a82cec9b47
⎯꯭̽𓆰𝅃꯭꯭᳚ ༎ࠫ ꯭𓆩꯭᪵𝛅͟𝗔͟ ̬𝗡᳟᪵͟𝗔᳟͟𝗧͟𝗔᳟᪵͟𝗡᳟͟𝗜 ͟𓆪᪵ꪾ ͟𝛊֟፝ؖ۬𝁘𝐁᧘ᴄʜ𝛊֟፝ؖ۬ <sangeeta271713@gmail.com>
2024-12-01 20:26:45 +0530

    Update live.py

--- VIPMUSIC/plugins/play/live.py
+++ VIPMUSIC/plugins/play/live.py
@@ -8,10 +8,13 @@
 # All rights reserved.
 #
 
+# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
+# Owner https://t.me/ll_BAD_MUNDA_ll
+
 from pyrogram import filters
 
 from config import BANNED_USERS
-from VIPMUSIC import YouTube, app
+from VIPMUSIC import Platform, app
 from VIPMUSIC.utils.channelplay import get_channeplayCB
 from VIPMUSIC.utils.decorators.language import languageCB
 from VIPMUSIC.utils.stream.stream import stream
@@ -26,24 +29,24 @@ async def play_live_stream(client, CallbackQuery, _):
     if CallbackQuery.from_user.id != int(user_id):
         try:
             return await CallbackQuery.answer(_["playcb_1"], show_alert=True)
-        except:
+        except Exception:
             return
     try:
         chat_id, channel = await get_channeplayCB(_, cplay, CallbackQuery)
-    except:
+    except Exception:
         return
     video = True if mode == "v" else None
     user_name = CallbackQuery.from_user.first_name
     await CallbackQuery.message.delete()
     try:
         await CallbackQuery.answer()
-    except:
+    except Exception:
         pass
     mystic = await CallbackQuery.message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
     try:
-        details, track_id = await YouTube.track(vidid, True)
+        details, track_id = await Platform.youtube.track(vidid, True)
     except Exception:
         return await mystic.edit_text(_["play_3"])
     ffplay = True if fplay == "f" else None
@@ -68,3 +71,4 @@ async def play_live_stream(client, CallbackQuery, _):
     else:
         return await mystic.edit_text("Not a live stream")
     await mystic.delete()
+            
