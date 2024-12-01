e2588bc1935b216aac96af357975296c7f086091
⎯꯭̽𓆰𝅃꯭꯭᳚ ༎ࠫ ꯭𓆩꯭᪵𝛅͟𝗔͟ ̬𝗡᳟᪵͟𝗔᳟͟𝗧͟𝗔᳟᪵͟𝗡᳟͟𝗜 ͟𓆪᪵ꪾ ͟𝛊֟፝ؖ۬𝁘𝐁᧘ᴄʜ𝛊֟፝ؖ۬ <sangeeta271713@gmail.com>
2024-12-01 20:21:47 +0530

    Update seek.py

--- VIPMUSIC/plugins/admins/seek.py
+++ VIPMUSIC/plugins/admins/seek.py
@@ -12,14 +12,13 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from VIPMUSIC import YouTube, app
+from VIPMUSIC import Platform, app
 from VIPMUSIC.core.call import VIP
 from VIPMUSIC.misc import db
 from VIPMUSIC.utils import AdminRightsCheck, seconds_to_min
 
-# Commands
+# Command
 SEEK_COMMAND = get_command("SEEK_COMMAND")
-
 
 @app.on_message(filters.command(SEEK_COMMAND) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
@@ -55,21 +54,22 @@ async def seek_comm(cli, message: Message, _, chat_id):
         to_seek = duration_played + duration_to_skip + 1
     mystic = await message.reply_text(_["admin_32"])
     if "vid_" in file_path:
-        n, file_path = await YouTube.video(playing[0]["vidid"], True)
+        n, file_path = await Platform.youtube.video(playing[0]["vidid"], True)
         if n == 0:
             return await message.reply_text(_["admin_30"])
     try:
-        await VIP.seek_stream(
+        await BAD.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
             duration,
             playing[0]["streamtype"],
         )
-    except:
+    except Exception:
         return await mystic.edit_text(_["admin_34"])
     if message.command[0][-2] == "c":
         db[chat_id][0]["played"] -= duration_to_skip
     else:
         db[chat_id][0]["played"] += duration_to_skip
     await mystic.edit_text(_["admin_33"].format(seconds_to_min(to_seek)))
+    
