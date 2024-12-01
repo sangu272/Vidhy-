d0d0c1d02d5bd6b0cf46b8284e2bf256d7ea3187
⎯꯭̽𓆰𝅃꯭꯭᳚ ༎ࠫ ꯭𓆩꯭᪵𝛅͟𝗔͟ ̬𝗡᳟᪵͟𝗔᳟͟𝗧͟𝗔᳟᪵͟𝗡᳟͟𝗜 ͟𓆪᪵ꪾ ͟𝛊֟፝ؖ۬𝁘𝐁᧘ᴄʜ𝛊֟፝ؖ۬ <sangeeta271713@gmail.com>
2024-12-01 22:09:16 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -13,6 +13,7 @@ import os
 import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+
 from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.dir import dirr
 from VIPMUSIC.core.git import git
@@ -21,21 +22,35 @@ from VIPMUSIC.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
+# Bot Client
 
+# Directories
 dirr()
 
+# Check Git Updates
 git()
 
+# Initialize Memory DB
 dbb()
 
+# Heroku APP
 heroku()
 
+# Load Sudo Users from DB
 sudo()
 
 app = VIPBot()
 
+# Assistant Client
 userbot = Userbot()
 
-from .platforms import platform 
-HELPABLE = {}
+from .platforms import *
 
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+HELPABLE = {}
